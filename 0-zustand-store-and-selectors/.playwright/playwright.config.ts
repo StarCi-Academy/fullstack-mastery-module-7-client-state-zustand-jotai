e2e081b986fb2d19eef8fce711fe8bde4ec5c16d
starci183 <cuongnvtse160875@gmail.com>
2026-05-26 22:36:30 +0700

    audit(M7L0,L1,L3): isolate @playwright/test, fix ports, playwright config

--- 0-zustand-store-and-selectors/.playwright/playwright.config.ts
+++ 0-zustand-store-and-selectors/.playwright/playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from "@playwright/test"
  */
 export default defineConfig({
     testDir: "./scripts",
-    timeout: 30_000,
+    timeout: 120_000,
     use: {
         baseURL: "http://localhost:3001",
         trace: "on-first-retry",
